d9a90bff9fc96d997d61ebbcf3ff3a2efdc09335
Akash Angadi <121341648+akash47angadi@users.noreply.github.com>
2024-03-04 19:03:04 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  court<<"Webhook successfully created\n";
+  cout<<"Webhook successfully created\n";
   return 0;
 }
